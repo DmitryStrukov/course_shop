88b8f4fa2be0cff844005bb752cef374a98c27e7
DmitryStrukov <d.strukov@mail.ru>
2021-02-12 16:26:06 +0300

    add navbar and render

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.static('public'));
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index', {
+    title: 'Главная страница',
+    isHome: true,
+  });
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'Добавить курс',
+    isAdd: true,
+  });
 });
+
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Список курсов',
+    isCourses: true,
+  });
+});
+
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
