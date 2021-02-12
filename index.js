2be5d8b298521bd921033afaa0a29bb90d8b54c6
DmitryStrukov <d.strukov@mail.ru>
2021-02-12 13:44:49 +0300

    add first html

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
